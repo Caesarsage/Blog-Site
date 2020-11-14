b8cc5b6d3e75c7a07eef3396e37fc82f061e381e
Destiny Erhabor <destinterhabor6@gmail.com>
2020-11-14 13:25:54 +0100

    review routes

--- routes/reviewRouters.js
+++ routes/reviewRouters.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const Review = require('../models/reviewModel');
 const Blog = require('../models/blogModel');
+const catchAsync = require('../utils/catchAsync');
 
 const reviewRouter = express.Router({mergeParams: true});
 
 reviewRouter.route('/')
-.post(async(req,res)=>{
+.post( catchAsync(async(req,res)=>{
   const { id } = req.params;
   const blogs =await Blog.findById(id)
   const review = new Review(req.body.review);
@@ -13,6 +14,13 @@ reviewRouter.route('/')
   await review.save();
   await blogs.save();
   res.redirect(`/blogs/${blogs._id}`);
-})
+}))
+
+reviewRouter.route('/:reviewId').delete( catchAsync(async(req, res)=>{
+ const { id, reviewId} = req.params;
+  const blogs = await Blog.findByIdAndUpdate(id, { $pull: { reviews: reviewId }})
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/blogs/${blogs._id}`)
+}))
 
 module.exports = reviewRouter
